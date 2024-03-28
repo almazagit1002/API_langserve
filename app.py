0f24b525f862c78b2a286540fdf57a409f277886
Ale Maza <almazagit1002@gmail.com>
2024-03-28 12:53:56 +0000

    simple api working

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
+import uvicorn
 from dotenv import load_dotenv
 import os
-from typing import List
 from pinecone import Pinecone
 from langchain_openai import ChatOpenAI
-from langserve import add_routes
 from langchain.agents import AgentExecutor, create_openai_functions_agent
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import Pinecone as LCPC
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain.tools.retriever import create_retriever_tool
-from langchain.pydantic_v1 import BaseModel, Field
-from langchain_core.messages import BaseMessage
+from langchain.pydantic_v1 import BaseModel
 from langchain import hub
 
 
 
-llm = ChatOpenAI(model='gpt-3.5-turbo',temperature=0)
 load_dotenv()
+
+model = ChatOpenAI(model='gpt-3.5-turbo',temperature=0)
+
+
 
 def query_pinecone():
     """Look up for product managment info."""
@@ -48,33 +50,23 @@ web_search = TavilySearchResults()
 tools = [retriever_tool,web_search]
 
 prompt = hub.pull("hwchase17/openai-functions-agent")
-agent = create_openai_functions_agent(llm,tools,prompt)
+print(prompt.messages)
+agent = create_openai_functions_agent(model,tools,prompt)
+
 agent_excecutor =AgentExecutor(agent=agent,tools=tools,verbose = True)
 
-class Input(BaseModel):
-    input: str
-    chat_history: List[BaseMessage] = Field(
-        ...,
-        extra={"widget": {"type": "chat", "input": "location"}},
-    )
-class Output(BaseModel):
-    output: str
 
 
 
-app=FastAPI(
-    title="Langchain Server",
-    version="1.0",
-    decsription="Hepha API"
+app = FastAPI()
 
-)
 
-add_routes(
-    app,
-    agent_excecutor.with_types(input_type=Input,output_type=Output),
-    path="/hepha"
-)
-
+@app.post("/Hepha_api")
+async def modify_string(input_string: str):
+    print(type(input_string))
+    print(input_string)
+    result = agent_excecutor.invoke({'input':input_string})
+    return result['output']
 
 if __name__=="__main__":
     uvicorn.run(app,host="localhost",port=8000)
--- client.py
+++ client.py
@@ -1,7 +1,18 @@
 import requests
 
-response=requests.post(
-    "http://localhost:8000/hepha/invoke",
-    json={'input':{'topic':"What is the weather in london"}})
+# URL of the FastAPI server
+url = "http://localhost:8000/Hepha_api"
 
-print(response)
+# String to be modified
+input_string = "weather in london"
+url_with_param = f"{url}?input_string={input_string}"
+
+
+response = requests.post(url_with_param)
+
+# Check if the request was successful
+if response.status_code == 200:
+    modified_string = response.text
+    print("Answer:", modified_string)
+else:
+    print("Error:", response.text)
